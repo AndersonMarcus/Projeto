26e2fad507399cc4a359092caeae89085ad773d1
Anderson Marcus <andrsn@hotmail.com>
2023-05-01 23:19:51 -0300

    Novas alterações

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from PyQt5.QtWidgets import QApplication, QMainWindow
 from PyQt5 import QtWidgets, uic
 from finance import Ui_MainWindow
 from termcolor import colored
+import time
 
 
 class financas(QMainWindow):
@@ -16,41 +17,55 @@ class financas(QMainWindow):
       self.valor_total = 0
       self.valor_saiu = 0
       self.linha = 0
-
+      self.apagar()
+      
+   
+    def apagar(self):
+        self.ui.frame_error.hide()  
+        
     def adicionar(self):
-
         self.descricao = self.ui.lineEdit.text()
         self.valor = self.ui.lineEdit_2.text()
+        
+        if  self.descricao == "" or self.valor == "":
+            self.ui.frame_error.show()
+            self.ui.label_error.setText("Erro! Campo(s) vazios(s)") 
+            
+        elif not self.valor.isnumeric():    
+            self.ui.frame_error.show()
+            self.ui.label_error.setText("Erro! Somente numero(s)") 
+        else:
+            self.apagar()
+    
+            if self.ui.radioButton.isChecked():
 
-        if self.ui.radioButton.isChecked():
+                self.valor_entrou = float(self.valor_entrou) + float(self.valor)
+                self.valor_total = float(self.valor_total) + float(self.valor)
 
-            self.valor_entrou = int(self.valor_entrou) + int(self.valor)
-            self.valor_total = int(self.valor_total) + int(self.valor)
+                self.ui.label_7.setText(f'R$ {self.valor_entrou:>.2f}'.replace('.', ','))
+                self.ui.label_9.setText(f'R$ {self.valor_total:>.2f}'.replace('.', ','))
+                self.ui.lineEdit.setText("")
+                self.ui.lineEdit_2.setText("")
 
-            self.ui.label_7.setText(f'R$ {self.valor_entrou:>1.2f}'.replace('.', ','))
-            self.ui.label_9.setText(f'R$ {self.valor_total:>1.2f}'.replace('.', ','))
-            self.ui.lineEdit.setText("")
-            self.ui.lineEdit_2.setText("")
+                self.tipo = 'Entrada '
 
-            self.tipo = 'Entrada '
+            if self.ui.radioButton_2.isChecked():
 
-        if self.ui.radioButton_2.isChecked():
+                self.valor_saiu = float(self.valor_saiu) + float(self.valor)
+                self.valor_total = float(self.valor_total) - float(self.valor)
 
-            self.valor_saiu = float(self.valor_saiu) + float(self.valor)
-            self.valor_total = float(self.valor_total) - float(self.valor)
+                self.ui.label_8.setText(f'R$ {self.valor_saiu:>.2f}'.replace('.', ','))
+                self.ui.label_9.setText(f'R$ {self.valor_total:>.2f}'.replace('.', ','))
 
-            self.ui.label_8.setText(f'R$ {self.valor_saiu:.3f}')
-            self.ui.label_9.setText(f'R$ {self.valor_total:.3f}')
+                self.tipo = 'Saída'
 
-            self.tipo = 'Saída'
+            self.ui.tableWidget.setRowCount(self.linha+1)
+            self.ui.tableWidget.setColumnCount(3)
+            self.ui.tableWidget.setItem(self.linha,0,QtWidgets.QTableWidgetItem(f'{self.descricao}'))
+            self.ui.tableWidget.setItem(self.linha,1,QtWidgets.QTableWidgetItem(f' R$ {float(self.valor):>.2f}'.replace('.', ',')))
+            self.ui.tableWidget.setItem(self.linha,2,QtWidgets.QTableWidgetItem(f'{self.tipo}'))
 
-        self.ui.tableWidget.setRowCount(self.linha+1)
-        self.ui.tableWidget.setColumnCount(3)
-        self.ui.tableWidget.setItem(self.linha,0,QtWidgets.QTableWidgetItem(f'{self.descricao}'))
-        self.ui.tableWidget.setItem(self.linha,1,QtWidgets.QTableWidgetItem(f'{self.valor}'))
-        self.ui.tableWidget.setItem(self.linha,2,QtWidgets.QTableWidgetItem(f'{self.tipo}'))
-
-        self.linha = self.linha + 1
+            self.linha = self.linha + 1
 
 
 if __name__ == "__main__":
